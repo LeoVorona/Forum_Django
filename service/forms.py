5f4a396049ce973667e52ecd302a46d2d9f2e214
leoflex <voronaleonid@mail.ru>
2022-08-22 18:42:15 +0300

    add login

--- service/forms.py
+++ service/forms.py
@@ -3,6 +3,9 @@ from pyexpat import model
 from django.forms import ModelForm
 from .models import Comment, Post
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class PostForm(ModelForm):
     class Meta:
@@ -21,3 +24,8 @@ class CommentForm(ModelForm):
         vidgets = {
             'description': forms.Textarea(attrs={'class':'form-control'}),
             }
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'email']
