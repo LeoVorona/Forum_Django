975053b29dea230f9fa90fb2a44a5444fa4129b3
leoflex <voronaleonid@mail.ru>
2022-08-23 14:25:05 +0300

    save changes

--- forum_prj/settings.py
+++ forum_prj/settings.py
@@ -124,7 +124,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
